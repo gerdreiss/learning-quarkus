fd4036ea8cf421c20080df93825d8a65caa67525
Gerd Reiss <gerd@reiss.pro>
2026-06-14 09:37:53 +0200

    Quarkus for Beginners - GET with cookies

--- udemy/quarkus-for-beginners/01-code-with-quarkus/src/main/java/org/acme/GamesResource.java
+++ udemy/quarkus-for-beginners/01-code-with-quarkus/src/main/java/org/acme/GamesResource.java
@@ -3,9 +3,11 @@ package org.acme;
 import io.vavr.collection.List;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import org.acme.entity.Game;
 
+import static java.util.function.Function.identity;
 
 @Path("/games")
 public class GamesResource {
@@ -13,21 +15,20 @@ public class GamesResource {
 
     public GamesResource() {
         games = List.of(
-                new Game(1L, "R6", "FPS"),
-                new Game(2L, "Battlefield 1", "FPS")
+            new Game(1L, "R6", "FPS"),
+            new Game(2L, "Battlefield 1", "FPS"),
+            new Game(3L, "Fallout 4", "RPM")
         );
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getGames(
-            @QueryParam("page") int page,
-            @QueryParam("size") int size,
-            @QueryParam("name") String name
+        @QueryParam("page") int page,
+        @QueryParam("size") int size,
+        @QueryParam("name") String name,
+        @CookieParam("gameCategory") String gameCategory
     ) {
-
-        System.out.printf("GamesResource.getGames: page = %d; size = %d; name = '%s%n'", page, size, name);
-
         List<Game> pagedGames = games;
         if (name != null && !name.isEmpty()) {
             pagedGames = pagedGames.filter(g -> g.name().toLowerCase().contains(name.toLowerCase()));
@@ -41,7 +42,14 @@ public class GamesResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        java.util.List<Game> result = pagedGames.subSequence(start, end).toJavaList();
+        var result = pagedGames.subSequence(start, end).sortBy(
+            (o1, o2) -> {
+                if (o1.category().equalsIgnoreCase(gameCategory)) return -1;
+                if (o2.category().equalsIgnoreCase(gameCategory)) return 1;
+                return 0;
+            },
+            identity()
+        ).toJavaList();
         return Response.ok(result).header("X-Total-Count", total).build();
     }
 
@@ -50,11 +58,21 @@ public class GamesResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getGame(@PathParam("id") long id) {
         return games
-                .find(g -> g.id() == id)
-                .fold(
-                        () -> Response.status(Response.Status.NOT_FOUND).build(),
-                        g -> Response.ok(g).build()
-                );
+            .find(g -> g.id() == id)
+            .fold(
+                () -> Response.status(Response.Status.NOT_FOUND).build(),
+                g -> Response.ok(g)
+                    .cookie(
+                        new NewCookie.Builder("gameCategory")
+                            .value(g.category())
+                            .path("/")
+                            .comment("Games category")
+                            .maxAge(3600)
+                            .build()
+                    )
+                    .build()
+            );
 
     }
+
 }
