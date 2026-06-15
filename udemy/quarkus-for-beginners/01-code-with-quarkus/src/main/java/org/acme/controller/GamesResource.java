6ef7ae47611cdbc862f08bf23d6ee36e489121f5
Gerd Reiss <gerd@reiss.pro>
2026-06-15 12:34:32 +0200

    Quarkus for Beginners - inject service

--- udemy/quarkus-for-beginners/01-code-with-quarkus/src/main/java/org/acme/controller/GamesResource.java
+++ udemy/quarkus-for-beginners/01-code-with-quarkus/src/main/java/org/acme/controller/GamesResource.java
@@ -1,12 +1,13 @@
-package org.acme;
+package org.acme.controller;
 
-import io.vavr.collection.List;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import org.acme.model.Game;
 import org.acme.model.NewGame;
+import org.acme.service.GameService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -17,23 +18,13 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import java.net.URI;
 import java.util.Map;
 
-import static java.util.function.Function.identity;
-
 @Path("/games")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "Game controller")
 public class GamesResource {
-    private List<Game> games;
-
-    public GamesResource() {
-        games = List.of(
-            new Game(1L, "Metro 2033", "FPS"),
-            new Game(2L, "S.T.A.L.K.E.R.: Clear Sky", "FPS"),
-            new Game(3L, "Fallout 4", "FPS"),
-            new Game(4L, "AtomRPG", "RPG")
-        );
-    }
+    @Inject
+    private GameService gameService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -57,36 +48,14 @@ public class GamesResource {
         @HeaderParam("page") int page,
         @HeaderParam("size") int size,
         @QueryParam("name") String name,
-        @CookieParam("gameCategory") String gameCategory
+        @CookieParam("gameCategory") String category
     ) {
-        List<Game> pagedGames = games;
-        if (name != null && !name.isEmpty()) {
-            pagedGames = pagedGames.filter(g -> g.name().toLowerCase().contains(name.toLowerCase()));
-        }
-
-        int total = pagedGames.size();
-        int start = (page - 1) * size;
-        int end = Math.min(start + size, total);
-
-        if (start >= total) {
-            int statusCode = Response.Status.NOT_FOUND.getStatusCode();
-            String reasonPhrase = "Page %d exceed available pages with size %d".formatted(page, size);
-            return Response.status(statusCode, reasonPhrase).build();
-        }
-
-        var result = pagedGames
-            .subSequence(start, end)
-            .sortBy(
-                (o1, o2) -> {
-                    if (o1.category().equalsIgnoreCase(gameCategory)) return -1;
-                    if (o2.category().equalsIgnoreCase(gameCategory)) return 1;
-                    return 0;
-                },
-                identity()
-            )
-            .toJavaList();
-
-        return Response.ok(result).header("X-Total-Count", total).build();
+        return gameService
+            .getGames(name, category, page, size)
+            .fold(
+                reasonPhrase -> Response.status(Response.Status.NOT_FOUND.getStatusCode(), reasonPhrase).build(),
+                games -> Response.ok(games.toJavaList()).build()
+            );
     }
 
     @GET
@@ -109,8 +78,8 @@ public class GamesResource {
         )
     )
     public Response getGame(@PathParam("id") long id) {
-        return games
-            .find(g -> g.id() == id)
+        return gameService
+            .getGame(id)
             .fold(
                 () -> Response.status(Response.Status.NOT_FOUND).build(),
                 g -> Response
@@ -143,8 +112,7 @@ public class GamesResource {
         )
     )
     public Response createGame(NewGame newGame) {
-        var newId = games.map(Game::id).max().fold(() -> 1L, id -> id + 1);
-        games = games.append(new Game(newId, newGame.name(), newGame.category()));
+        var newId = gameService.createGame(newGame.name(), newGame.category());
         return Response.created(URI.create("/games/" + newId)).build();
     }
 
@@ -157,16 +125,11 @@ public class GamesResource {
         description = "Patches an existing game."
     )
     public Response updateGame(@PathParam("id") long id, Map<String, String> update) {
-        return games.find(g -> g.id() == id)
+        return gameService
+            .updateGame(id, update)
             .fold(
                 () -> Response.status(Response.Status.NOT_FOUND).build(),
-                g -> {
-                    String newName = update.getOrDefault("name", g.name());
-                    String newCategory = update.getOrDefault("category", g.category());
-                    var updatedGame = new Game(id, newName, newCategory);
-                    games = games.replace(g, updatedGame);
-                    return Response.ok(updatedGame).build();
-                }
+                g -> Response.ok(g).build()
             );
     }
 
@@ -179,14 +142,11 @@ public class GamesResource {
         description = "Replaces a game for the given ID."
     )
     public Response replaceGame(@PathParam("id") long id, NewGame gameDTO) {
-        return games.find(g -> g.id() == id)
+        return gameService
+            .updateGame(id, Map.of("name", gameDTO.name(), "category", gameDTO.category()))
             .fold(
                 () -> Response.status(Response.Status.NOT_FOUND).build(),
-                g -> {
-                    var newGame = new Game(id, gameDTO.name(), gameDTO.category());
-                    games = games.replace(g, newGame);
-                    return Response.ok(newGame).build();
-                }
+                g -> Response.ok(g).build()
             );
     }
 
@@ -198,7 +158,6 @@ public class GamesResource {
         description = "Deletes a game for the given ID."
     )
     public Response deleteGame(@PathParam("id") long id) {
-        games = games.removeFirst(g -> g.id() == id);
         return Response.noContent().build();
     }
 
---
+++ udemy/quarkus-for-beginners/01-code-with-quarkus/src/main/java/org/acme/service/GameService.java
@@ -0,0 +1,81 @@
+package org.acme.service;
+
+
+import io.vavr.collection.List;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import jakarta.enterprise.context.Dependent;
+import org.acme.model.Game;
+
+
+import java.util.Map;
+
+import static java.util.function.Function.identity;
+
+@Dependent
+public class GameService {
+    private List<Game> games;
+
+    public GameService() {
+        games = List.of(
+            new Game(1L, "Metro 2033", "FPS"),
+            new Game(2L, "S.T.A.L.K.E.R.: Clear Sky", "FPS"),
+            new Game(3L, "Fallout 4", "FPS"),
+            new Game(4L, "AtomRPG", "RPG")
+        );
+    }
+
+    public Either<String, List<Game>> getGames(String name, String gameCategory, int page, int size) {
+        List<Game> pagedGames = games;
+        if (name != null && !name.isEmpty()) {
+            pagedGames = pagedGames.filter(g -> g.name().toLowerCase().contains(name.toLowerCase()));
+        }
+
+        int total = pagedGames.size();
+        int start = (page - 1) * size;
+        int end = Math.min(start + size, total);
+
+        if (start >= total) {
+            return Either.left("Page %d exceed available pages with size %d".formatted(page, size));
+        }
+
+        var result = pagedGames
+            .subSequence(start, end)
+            .sortBy(
+                (o1, o2) -> {
+                    if (o1.category().equalsIgnoreCase(gameCategory)) return -1;
+                    if (o2.category().equalsIgnoreCase(gameCategory)) return 1;
+                    return 0;
+                },
+                identity()
+            );
+
+        return Either.right(result);
+    }
+
+    public Option<Game> getGame(Long id) {
+        return games.find(g -> id == g.id());
+    }
+
+    public Long createGame(String name, String category) {
+        var newId = games.map(Game::id).max().fold(() -> 1L, id -> id + 1);
+        games = games.append(new Game(newId, name, category));
+        return newId;
+    }
+
+    public Option<Game> updateGame(Long id, Map<String, String> update) {
+        return games
+            .find(g -> g.id() == id)
+            .map(g -> {
+                String newName = update.getOrDefault("name", g.name());
+                String newCategory = update.getOrDefault("category", g.category());
+                var updatedGame = new Game(id, newName, newCategory);
+                games = games.replace(g, updatedGame);
+                return updatedGame;
+            });
+    }
+
+    public void deleteGame(Long id) {
+        games = games.removeFirst(g -> g.id() == id);
+    }
+}
